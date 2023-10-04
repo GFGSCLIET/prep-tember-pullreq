43fa0b146de5323d4dc0bb86c16a774a1316779b
Sree <tejasreemaddi3@gmail.com>
2023-10-04 07:34:54 +0530

    added solution_question4.c

---
+++ code-debugging/solution_question4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+  int main(){
+  int x=1.1;
+  switch(x)
+  {
+   case 1: printf("Choice is 1");
+    break;
+   default: printf("Invalid Choice");
+    break;
+   }
+return 0;
+}   
+
+//Output: Choice is 1
